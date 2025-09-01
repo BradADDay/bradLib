6ac56b3c517e458f4b1bc9323fdb48aebdbf491e
Bradley Day <braday2005@gmail.com>
2025-09-01 20:33:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="bradLib",
-    version="0.0.0",
+    version="1.1.0",
     license="MIT",
     description="""Bradley Day's personal library""",
     long_description=long_description,
