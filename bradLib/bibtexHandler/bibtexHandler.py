7bed7b451b1a607d4ab15c954764392abf6907ec
Brad <braday2005@gmail.com>
2026-04-28 23:21:53 +0100

    commented bibtexHandler

--- bradLib/bibtexHandler/bibtexHandler.py
+++ bradLib/bibtexHandler/bibtexHandler.py
@@ -6,116 +6,165 @@ import bibtexparser as bp
 import os
 from bradLib.fileHandler import jsonHandler
 
+# Getting the location of this file
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+# Reading the requirements file and taking fields from it
 requirements = jsonHandler.load(os.path.join(__location__, "requirements.json"))
-keys = list(requirements["article"].keys())
+fields = list(requirements["article"].keys())
+
+# Generate an empty bib csv
+def generateCSV(fields = fields):
+    """Generate an empty csv with a given list of headers"""
+    
+    df = pd.DataFrame(columns=fields, index=None)
+    df.to_csv("emptyBibliography.csv", index=False)
+    
     
 def cleanEmptyDict(dict):
+    """Remove empty entries from a dictionary"""
     
     newDict = {}
-    
     for key in dict.keys():
-        
         if dict[key] != "":
             newDict[key] = str(dict[key])
-    
     return newDict
     
 class bibEntry():
-    
-    def __init__(self, dict, ID, type, requirements, keys):
+    """A class to store single bibtex entries"""
+    def __init__(self, dict, ID, type, requirements, fields):
         
+        # Initialising an empty dictionaryy
         self.dict = {}
+        
+        # Storing the ID and type of the entry
         self.ID = ID
         self.type = type
         
-        for key in keys:
-            try:
-                self.dict[key] = dict[key]
-            except:
-                self.dict[key] = ""
+        # Storing the fields of the entry in the dictionary
+        for field in fields:
+            if field in dict.keys():
+                self.dict[field] = dict[field]
+            else:
+                self.dict[field] = ""
         
+        # Checking for missing necessary entries
         self.checkMissing(requirements[self.type])
         
+        # Storing the ID and type
         self.dict["ID"] = self.ID
         self.dict["ENTRYTYPE"] = self.type
         
+        # Cleaning the dictionary
         self.cleanDict = cleanEmptyDict(self.dict)
         
     def checkMissing(self, requirements):
+        """Check for missing fields that are required as defined in requirements.json"""
         
+        # Checking the required fields
         for field in requirements.keys():
-            
             if (requirements[field] == "required") and (self.dict[field] == ""):
                 
+                # Printing if a missing field is found and setting its value to "MISSING ENTRY"
                 print(f"Entry {self.ID} missing field {field}")
                 self.dict[field] = "MISSING ENTRY"
         
 class bibliography():
+    """A class for storing an entire bibliography"""
     
+    # Storing the requirements dict and fields 
     requirements = requirements
-    keys = keys
+    fields = fields
     
     def __init__(self, filePath):
         
+        # Storing the file path and extension
         self.filePath = filePath
         self.fileType = filePath[-3:]
+        
+        # Creating a list to store the entries
         self.entries = []
         
+        # Checking the filetype and loading
         if self.fileType == "csv":
             self.fromCSV(filePath)
         elif self.fileType == "bib":
             self.fromBIB(filePath)
         else:
-            raise ValueError("Unkown file type entered, please fix.")
+            raise ValueError("File of unknown type entered, accepted types are '.csv' and '.bib' please fix.")
             
     def fromCSV(self, filePath):
+        """Load bib data from csv"""
         
+        # Reading the csv
         df = pd.read_csv(filePath, na_filter=False)
         
+        # Instantiating all entries as bibEntry objects
         for i in range(len(df)):
             entry = df.iloc[i].to_dict()
-            key = entry.pop("key")
-            type = entry.pop("type")
-            self.entries.append(bibEntry(entry, key, type, self.requirements, self.keys))
+            
+            # Instantiating the entry
+            self.instantiateEntry(entry)
     
     def fromBIB(self, filePath):
+        """Load bib data from bib"""
         
+        # Reading the bib file
         with open(filePath) as file:
             fileStr = file.read()
         
+        # Loading as a bibtexparser library
         bib = bp.loads(fileStr)
         
+        # Instantiating all entries as bibEntry objects
         for entry in bib.entries:
-            key = entry.pop("ID")
-            type = entry.pop("ENTRYTYPE")
-            self.entries.append(bibEntry(entry, key, type, self.requirements, self.keys))
             
-    def toCSV(self, filePath):
+            # Instantiating the entry
+            self.instantiateEntry(entry)
+            
+    def instantiateEntry(self, entry):
         
+        # Getting the ID and type
+        try: ID = entry.pop("ID")
+        except: raise ValueError(f"ID missing, please check input data for entry {entry}")
+        try: type = entry.pop("ENTRYTYPE")
+        except: raise ValueError(f"Type missing, please check input data for entry {entry}")
+        
+        # Adding the entry to the list
+        self.entries.append(bibEntry(entry, ID, type, self.requirements, self.fields))
+        
+    def toCSV(self, filePath):
+        """Save the bibliography as a csv"""
+        
+        # The order for the columns
         order = ["ID", "ENTRYTYPE", "title", "author", "year", "journal", "doi", 
                  "url", "number", "pages", "publisher", "address", "annote", 
                  "booktitle", "crossref", "edition", "editor", "howpublished", 
                  "institution", "month", "note", "organization", "school", 
                  "series", "issn", "isbn", "eprint"]
         
-        df = pd.DataFrame(columns = self.keys)
+        # Instantiating the dataframe and ordering it
+        df = pd.DataFrame(columns = self.fields)
         df = df.reindex(order, axis=1)
         
+        # Loading the entries into the dataframe
         for entry in self.entries:
-            
             df.loc[len(df)] = entry.cleanDict
         
+        # Saving the dataframe as csv
         df.to_csv(filePath, index=False)
     
     def toBIB(self, filePath):
+        """Save the bibliography as a bib"""
         
+        # Initialising list for storage
         entryList = []
         
+        # Storing the entry dictionaries
         for entry in self.entries:
             entryList.append(entry.cleanDict)
         
+        # Creating a bibdatabase object to save the bib file
         bib = bp.bibdatabase.BibDatabase()
         bib.entries = entryList
         
@@ -123,21 +172,3 @@ class bibliography():
         
         with open(filePath, "w") as bibfile:
             bibfile.write(writer.write(bib))
-
-def generateCSV(keys = keys):
-    df = pd.DataFrame(columns=keys, index=None)
-    df.to_csv("emptyBibliography.csv", index=False)
-    
-def startIO():
-    IN = str(input("Input file name: "))
-    choice = input("(0) To csv\n(1) To bib\nEnter Selection: ")
-    OUT = str(input("Output file name: "))
-    
-    bib = bibliography(IN)
-    
-    if int(choice) == 0:
-        bib.toCSV(OUT)
-    elif int(choice) == 1:
-        bib.toBIB(OUT)
-    else:
-        print("Something went wrong")
--- bradLib/bibtexHandler/start.py
+++ bradLib/bibtexHandler/start.py
@@ -1,4 +1,19 @@
 
-from bradLib.bibtexHandler import startIO
+import bibtexHandler
 
-startIO()
+"""Start the input-output for CLI use"""
+
+# Taking the input and output file paths
+IN = str(input("Input file name: "))
+OUT = str(input("Output file name: "))
+
+# Instantiating a bibliography object
+bib = bibtexHandler.bibliography(IN)
+
+# Saving the file as a csv or bib depending on the output filename
+if OUT[-3:] == "csv":
+    bib.toCSV(OUT)
+elif OUT[-3:] == "bib":
+    bib.toBIB(OUT)
+else:
+    raise ValueError("Something went wrong, please check the input filenames")
